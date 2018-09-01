edde642a1a5ae029dd86f0f59dcf24f657cb1635
Hugo Armenta <h.alexarmenta@gmail.com>
2018-08-31 19:12:53 -0500

    fix(circleci): change docker volume structure from image to adapt to ci

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,8 +12,6 @@ set -e
 : "${APP_SETUP_LOCK:="$APP_TEMP_PATH/setup.lock"}"
 : "${APP_SETUP_WAIT:="5"}"
 
-chown -R condovive:condovive $APP_PATH
-
 # 1: Define the functions lock and unlock our app containers setup processes:
 lock_setup() { mkdir -p "$APP_TEMP_PATH" && touch "$APP_SETUP_LOCK"; }
 unlock_setup() { rm -rf "$APP_SETUP_LOCK"; }
